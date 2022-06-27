7ce66339a0fc3d4de8f7a3c64c0b71529304a525
chocot-j <joy01010@naver.com>
2022-06-27 14:36:30 +0900

    paginator jump 10

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -27,8 +27,9 @@ def index(request):
     # 페이징 처리
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
+    end_page = len(paginator.page_range)
     
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    context = {'question_list': page_obj, 'page': page, 'end_page':end_page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 
